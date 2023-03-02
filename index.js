ccd168241b00eefe3863f008632f4c6169836756
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-02 19:10:04 -0400

    add criação de arquivo

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // Import all of the files and dependencies needed.
+import fs from 'fs'
 import express from 'express'
 import ws from './server/server.js'
 import db from './functions/database.js'
 import setup from './functions/setup.js'
 
-function main () {
+function main() {
     // Set up the initial systems (version control, database etc).
     setup(db)
 
     if (db.get('ready')) {
-    // Launch the server instance; including Express and our DB.
+        // Launch the server instance; including Express and our DB.
         ws(express(), db)
     }
+    setInterval(function () {
+        fs.writeFileSync("input.json", JSON.stringify(
+            `{"panel":"${db.get('panel')}","wings":"${db.get('wings')}","server":{"syncing":${db.get('ready')},"lastSynced":"${db.get('lastSynced')}"}}`
+            //`{"panel":"` + `${db.get('panel')}` + `","wings":"` + `${db.get('wings')}` + `","server":{"syncing":` + `${db.get('ready')}` + `,"lastSynced":"` + `${db.get('lastSynced')}` + `"}}`
+        ));
+    },
+        10000)
 }
 
 // Start our program here.
