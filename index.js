bd4254abf94ff3de3f3e67297eab2f3570fe256a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-07 10:53:06 -0400

    Correções

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-// Import all of the files and dependencies needed.
-import fs from 'fs'
-//import express from 'express'
-//import ws from './server/server.js'
-import db from './functions/database.js'
-import setup from './functions/setup.js'
+import fs from 'fs';
+import db from './functions/database.js';
+import setup from './functions/setup.js';
 
-setup(db)
+setup(db);
 
 setTimeout(function main() {
-console.log("Arquivo Criado")
-fs.writeFileSync("status.json", (`{
-  "panel": "${db.get('panel')}",
-  "wings": "${db.get('wings')}",
-  "server": {
-    "syncing": ${db.get('ready')},
-    "lastSynced": "${db.get('lastSynced')}"
-  }
-}`)
-)}, 13000)
- 
+  console.log("Criando arquivo...");
+  const data = {
+    panel: db.get('panel'),
+    wings: db.get('wings'),
+    server: {
+      syncing: db.get('ready'),
+      lastSynced: db.get('lastSynced')
+    }
+  };
+  // Validar e escapar os valores antes de inseri-los no JSON
+  const json = JSON.stringify(data, null, 2);
+  // Usar o método "fs.writeFile()" para não bloquear o processo principal
+  fs.writeFile("status.json", json, (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log("Arquivo criado com sucesso");
+    }
+  });
+}, 15000); // Tempo de 15 segundos
+
+// Fechar o banco de dados após 17 segundos
 setTimeout(function main() {
-db.close((err) => {
+  db.close((err) => {
     if (err) {
       console.error(err.message);
     } else {
       console.log('Banco de dados parado com sucesso');
     }
-  })}, 15000
-)
+  });
+}, 17000);
