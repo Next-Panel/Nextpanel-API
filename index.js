b6aef10ca255f9f73dcbb5efd2d42d65fc4c629c
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-11 21:57:13 -0400

    ok ok

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function createStatusFileJex(callback) {
     }
   });
   setTimeout(function main() {
-    db.set('panel', {}).write();
+    db.set('panel', undefined)
     db.close((err) => {
       if (err) {
         console.error(err.message);
