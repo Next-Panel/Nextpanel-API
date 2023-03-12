8a64d63247277c82aa4aafb60643b7981d89be0a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-11 21:18:31 -0400

    Correções

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import jexactyl from './functions/jexactyl.js';
 import pterodactyl from './functions/pterodactyl.js';
 
 function createStatusFileJex(callback) {
-  jexactyl(db);
+  try {
+    jexactyl(db);
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
   console.log("Criando arquivo...");
   const panel = db.get('panel');
   const wings = db.get('wings');
@@ -30,68 +35,48 @@ function createStatusFileJex(callback) {
   fs.writeFile("jexactyl.json", json, (err) => {
     if (err) {
       console.error(err.message);
+      callback(err);
+    } else {
+      console.log("Arquivo criado com sucesso");
+      // chamar a função createStatusFilePtero após a criação do arquivo jexactyl.json
+      createStatusFilePtero(callback);
+    }
+  });
+}
+
+function createStatusFilePtero(callback) {
+  console.log("Criando arquivo...");
+  const panel = db.get('panel');
+  const wings = db.get('wings');
+  const lastSynced = db.get('lastSynced');
+
+  // Verificar se algum valor é undefined
+  if (panel === undefined || wings === undefined || lastSynced === undefined) {
+    console.log("Algum valor é indefinido. Tentando novamente em 10 segundos...");
+    setTimeout(createStatusFilePtero, 10000, callback); // Chama a função novamente após 10 segundos
+    return;
+  }
+
+  const data = {
+    "daemon": "0.9.999",
+    panel,
+    wings,
+    "sftp": "1.0.5",
+    "discord": "https://discord.gg/Wf8Eycz4Tq",
+    lastSynced
+  };
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile("pterodactyl.json", json, (err) => {
+    if (err) {
+      console.error(err.message);
+      callback(err);
     } else {
       console.log("Arquivo criado com sucesso");
     }
   });
-
-  // Fechar o banco de dados após 2 segundos
-  setTimeout(function main() {
-    db.close((err) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log('Banco de dados parado com sucesso');
-        callback();
-      }
-    });
-  }, 2000);
-}
-
-function createStatusFilePtero(callback) {
-  pterodactyl(db, () => {
-    console.log("Criando arquivo...");
-    const panel = db.get('panel');
-    const wings = db.get('wings');
-    const lastSynced = db.get('lastSynced');
-
-    // Verificar se algum valor é undefined
-    if (panel === undefined || wings === undefined || lastSynced === undefined) {
-      console.log("Algum valor é indefinido. Tentando novamente em 10 segundos...");
-      setTimeout(createStatusFile, 10000); // Chama a função novamente após 10 segundos
-      return;
-    }
-
-    const data = {
-      "daemon": "0.9.999",
-      panel,
-      wings,
-      "sftp": "1.0.5",
-      "discord": "https://discord.gg/Wf8Eycz4Tq",
-      lastSynced
-    };
-    const json = JSON.stringify(data, null, 2);
-    fs.writeFile("pterodactyl.json", json, (err) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log("Arquivo criado com sucesso");
-      }
-    });
-
-    // Fechar o banco de dados após 2 segundos
-    setTimeout(function main() {
-      db.close((err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log('Banco de dados parado com sucesso');
-        }
-        callback(); // Chama o callback passado como parâmetro
-      });
-    }, 2000);
-  });
 }
 
 // Chama a função pela primeira vez
-createStatusFileJex(createStatusFilePtero);
+createStatusFileJex(() => {
+  console.log('Todas as funções foram executadas com sucesso.');
+});
